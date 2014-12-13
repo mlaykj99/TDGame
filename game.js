c381f04090ff85d937433f8a352074d7dd2de6dc
mlaykj99 <joshgreenwell779@yahoo.com>
2014-12-13 11:16:06 -0600

    Working on enemy movement.

--- game.js
+++ game.js
@@ -26,22 +26,14 @@ var currentRadiusTiles = [];
 //Towers
 var towerOptions =
 [
-    tower0 = createTower(1,null),
-    tower1 = createTower(2,null)//,
+    createTower(1,null),
+    createTower(2,null)//,
     //tower2 = createTower(3,null),
     //tower3 = createTower(4,null)
 ];
 
 //Temporary enemies for testing
-var enemies =
-[
-    enemy0 = {health: 50, speed: 5},
-    enemy1 = {health: 50, speed: 5},
-    enemy2 = {health: 50, speed: 5},
-    enemy3 = {health: 50, speed: 5},
-    enemy4 = {health: 0, speed: 5},
-    enemy5 = {health: 50, speed: 5}
-];
+var enemies = [];
 
 //current towerOptions on the map
 var currentTowers = [];
@@ -64,19 +56,80 @@ window.onload = function()
 
 function start()
 {
-    var running = true;
     var enemy;
+    var interval;
 
-    for(var i = 0; i < enemies.length; i++)
+    for(var i = 0; i < 10; i++)
     {
-        enemy = createHTMLElement('div', 'e'+i, 'enemy', 'e');
+        enemy = createHTMLElement('div', 'e'+i, 'enemy', '');
+        document.getElementById('board').innerHTML = document.getElementById('board').innerHTML + enemy;
+        enemies.push({health: 100, speed: 1, id: 'e'+i, interval: null});
+    }
 
+    positionEnemies();
+    for(i = 0; i < enemies.length; i++)
+    {
+        interval = setInterval(function(){moveEnemy(enemies[i])},100);
+        enemies[i].interval = interval;
     }
 
     for(i = 0; i < currentTowers.length; i++)
     {
         setInterval(function(){towerShoot(currentTowers[i])},10*currentTowers[i].speed);
     }
+}
+
+function positionEnemies()
+{
+    var top;
+    var left = -5;
+
+    top = (invalidSpaces[0].substring(1, invalidSpaces[0].indexOf('c'))) * 5.2; // * tile height
+
+    //moves the enemy divs to be in a straight line off the board
+    for(var i = 0; i < enemies.length; i++)
+    {
+        document.getElementById('e'+i).style.top = top+'vh';
+        document.getElementById('e'+i).style.left = left+'vh';
+        document.getElementById('e'+i).style.visibility = 'hidden';
+    }
+}
+
+function moveEnemy(enemy)
+{
+    var dead = false;// = enemyDead(enemy);
+    var element = document.getElementById(enemy.id);
+    var left = Number(element.style.left.substring(0,element.style.left.indexOf('vh')));
+
+    if(left === 0){element.style.visibility = 'visible';}
+    if(left >= 73){enemy.health = 0; dead = true;}
+    if(dead)
+    {
+        alert(enemies);
+        enemies.splice(0);
+        alert(enemies);
+        clearInterval(enemy.interval);
+        return;
+    }
+
+    //move enemy
+    element.style.left = left+1+'vh';
+}
+
+function enemyDead(enemy)
+{
+    var i;
+    var dead = false;
+
+    if(enemy.health <= 0)
+    {
+        for(i = 0; i < enemies.length; i++)
+        {
+            if(enemies[i] === enemy){break;}
+        }
+    }
+
+    return {dead: dead, index: i};
 }
 
 function towerShoot(tower)
